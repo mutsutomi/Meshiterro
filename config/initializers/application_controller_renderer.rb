7bdaf47a403ec4a7a67b83c93d84456d03b9a12a
mutsutomi <m2.tmk@ezweb.ne.jp>
2021-08-19 17:18:39 +0000

    finish3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '17c29d53fe6ee9d0ec26aa8f00b056c4a84c284997cc636c301be2dfcdd0102755c58312223d91e1f789ced11606a3b65a9dd60a7c9bb5d4a1eba6133910a3c2'
